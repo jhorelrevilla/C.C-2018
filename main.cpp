55aba98f43bfc5d9566eb63d169ca8bc2e8049d1
jhorel <jhorel.revilla@ucsp.edu.pe>
2018-04-02 18:26:27 -0500

    21.3.18

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*int x;
+    double y;
+    cout<<"dato de x: ";cin>>x;
+    cout<<"dato de y: ";cin>>y;
+    /*cout<<"Suma: "<<x+y<<endl;
+    cout<<"Resta: "<<x-y<<endl;
+    cout<<"Multiplicacion: "<<x*y<<endl;
+    cout<<"division: "<<x/y<<endl;
+
+ ////////////////////////////////////////////////////
+ ////////////////////////////////////////////////////
+    double P=3.14;
+    int x;
+    cout<<"radio: ";cin>>x;
+    cout<<"\""<<P*(x*x)<<"\""<<endl;
+    */
+    cout<<"*********\n*       *\n*       *\n*       *\n*       *\n*       *\n*       *\n*       *\n*       *\n*       *\n*       *\n*********"<<endl;
+    cout<<" "<<endl;
+    cout<<"  ***  \n *   * \n*     *\n*     *\n*     *\n*     *\n*     *\n *   * \n  ***  "<<endl;
+    cout<<" "<<endl;
+    cout<<"  *  \n *** \n*****\n  *  \n\n  *  \n\n  *  \n\n  *  \n\n  *  \n\n  *  \n\n  *  \n"<<endl;
+    return 0;
+}
