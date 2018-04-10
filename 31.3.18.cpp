0c4aeff555882f0a9135d2a2f94b48429a7a226d
jhorelrevilla <jhorel.revilla@ucsp.edu.pe>
2018-04-09 20:29:14 -0500

    Update 31.3.18.cpp

--- 31.3.18.cpp
+++ 31.3.18.cpp
@@ -2,9 +2,8 @@
 
 using namespace std;
 
-int main()
-{
-    //////////1//////////////
+
+void calcuedad(){
     int edad;
     cout<<"ingresa la edad: ";cin>>edad;
     if(edad>0  && edad<30){
@@ -13,9 +12,11 @@ int main()
     if(edad>=30){
         cout<<"es una persona mayor"<<endl;
     }
-    //////////2//////////////
+}
+
+void impnum(){
     int numhf;
-    cout<<"numero: ";cin>>numhf;
+    cout<<"numero para imprimir: ";cin>>numhf;
     for(int x=1;x<=numhf;++x){
         if(x<numhf){
             cout<<x<<",";
@@ -24,8 +25,9 @@ int main()
             cout<<x;
         }
     }
+}
 
-    //////////3//////////////
+void promnu(){
     int a;
     float n;
     int mayor=0;
@@ -33,7 +35,7 @@ int main()
     float prom=0;
     int cont=1;
     while(cont<=3){
-        cout<<"numero ";cin>>a;
+        cout<<"numero para promediar";cin>>a;
         if(a>mayor){
             mayor=a;
         }
@@ -46,7 +48,9 @@ int main()
     cout<<"mayor    "<<mayor<<endl;
     cout<<"menor    "<<menor<<endl;
     cout<<"promedio    "<<prom/3.0<<endl;
-    //////////4//////////////
+}
+
+void multi2(){
     int num1,num2;
     cout<<"ingrese el primer numero: ";cin>>num1;
     cout<<"ingrese el segundo numero: ";cin>>num2;
@@ -64,25 +68,40 @@ int main()
     if((num1*num1)==num2){
         cout<<"el primero al cuadrado es igual al segundo"<<endl;
     }
+}
 
-    //////////5//////////////
-    int prim;
-    cout<<"num primo: ";cin>>prim;
-
-    if(prim%2 && prim%3 && prim%5 && prim%7 && prim%11 || prim==2 || prim==3 || prim==5 || prim==7 || prim==11){
-        cout<<"es primo"<<endl;
-    }
-    else
-        cout<<"no es primo"<<endl;
-    //////////6//////////////
-    int num_prim;
-    cout<<"num de primos";cin>>num_prim;
-    for(int x=2;x<=num_prim;++x){
-        if(x%2 && x%3 && x%5 && x%7 && x%11 || x==2 || x==3 || x==5 || x==7 || x==11){
-            cout<<x<<" "<<endl;
+void prim(){
+    int a=0,i,n;
+    cout<<"Ingrese numero"<<endl;
+    cin>>n;
+    for(i=1;i<(n+1);i++){
+        if(n%i==0){
+            a++;
         }
     }
-    //////////7//////////////
+    if(a!=2){
+        cout<<"No es Primo"<<endl;
+    }else{
+        cout<<"Si es Primo"<<endl;
+    }
+}
+void return_prim(int n){
+    int a=0,i;
+    for(i=1;i<(n+1);i++){
+        if(n%i==0){
+            a++;
+        }
+    }
+    if(a==2){
+        cout<<n<<endl;
+    }
+}
+void numprim(int num){
+    for(int i=2;i<num;++i){
+        return_prim(i);
+    }
+}
+void numtab(){
     int numtab=0;
     cout<<"INGRESE EL NUMERO PARA TABULAR: ";cin>>numtab;
     if(numtab<=99999){
@@ -95,7 +114,8 @@ int main()
     }
     else
         cout<<"es un numero mayor a 5 digitos"<<endl;
-    //////////8//////////////
+}
+void palindrome(){
     int num_palin=0;
     int cont=0;
     cout<<"INGRESE EL NUMERO ";cin>>num_palin;
@@ -114,7 +134,8 @@ int main()
             cout<<"es palindrome"<<endl;
         }
      }
-    //////////9//////////////
+}
+void aniobi(){
     int aniobi;
     cout<<"ingresa año: ";cin>>aniobi;
 
@@ -123,7 +144,8 @@ int main()
     }
     else
         cout<<"no es bisiesto"<<endl;
-    //////////10//////////////
+}
+void fibona(){
     int fibo;
     int contant=1;
     int contact=0;
@@ -135,6 +157,21 @@ int main()
         contact=total;
     }
     cout<<total<<endl;
-
+}
+int main()
+{
+    int x;
+    calcuedad();
+    impnum();
+    cout<<"\n";
+    promnu();
+    multi2();
+    prim();
+    cout<<"calcular numero primo hasta: ";cin>>x;
+    numprim(x);
+    numtab();
+    palindrome();
+    aniobi();
+    fibona();
     return 0;
 }
