ae24d0653d2485ca26ee9cb4d6275400bd21ad09
jhorelrevilla <jhorel.revilla@ucsp.edu.pe>
2018-05-29 08:51:03 -0500

    Update charvirt.cpp

--- charvirt.cpp
+++ charvirt.cpp
@@ -41,6 +41,94 @@ void charvirt::agregar(int valor){
     delete [] a;
     a=nuevo;
 }
+///////////////////////////
+float charvirt::Promedio(){
+    float prom=0;
+    for(int i=0;i<tamanio;i++){
+        prom+=a[i];
+    }
+    return prom/tamanio;
+}
+void charvirt::ordenarnotas(){
+    int cont=1;
+    while(cont!=0){
+        cont=0;
+        for(int i=0;i<tamanio;i++){
+            if(a[i]>=a[i+1]){
+                float b=a[i];
+                a[i]=a[i+1];
+                a[i+1]=b;
+                cont+=1;
+            }
+        }
+    }
+}
+bool charvirt::prim(int a){
+    int prim1=0;
+    for(int i=1;i<=a;i++){
+        if(a%i==0){
+            prim1++;
+        }
+    }
+    if(prim1!=2){
+        return false;
+    }
+    else
+        return true;
+}
+int charvirt::MayorPrim(){
+    int mayp=0;
+    for(int i=0;i<tamanio;i++){
+        if(prim(a[i])){
+            mayp=a[i];
+        }
+    }
+    for(int i=0;i<tamanio;i++){
+        if(prim(a[i])){
+            if(a[i]>mayp){
+                mayp=a[i];
+            }
+        }
+    }
+    return mayp;
+}
+int charvirt::MenorPrim(){
+    int menp=0;
+    for(int i=0;i<tamanio;i++){
+        if(prim(a[i])){
+            menp=a[i];
+        }
+    }
+    for(int i=0;i<tamanio;i++){
+        if(prim(a[i])){
+            if(a[i]<menp){
+                menp=a[i];
+            }
+        }
+    }
+    return menp;
+}
+int charvirt::SumNotas(){
+    int sum=0;
+    for(int i=0;i<tamanio;i++){
+        sum+=a[i];
+    }
+    return sum;
+}
+void charvirt::invertir(){
+    int b;
+    for(int i=0;i<=(tamanio/2);i++){
+        if(i!=(tamanio-i-1)){
+            b=a[i];
+            a[i]=a[tamanio-i-1];
+            a[tamanio-i-1]=b;
+        }
+    }
+}
+//////////////////
+
+
+
 
 void charvirt::agregarpos(int elem, int pos){
     int *nuevo= new int [tamanio+1];
@@ -64,23 +152,6 @@ int charvirt::Obtenermayor(){
     }
     return mayor;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 charvirt::~charvirt(){
     delete []a;
